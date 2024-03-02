bf0233891a40de3d62bb90529ee99ee3f435562d
Githubuser289 <moldovan.mms@gmail.com>
2024-03-02 19:50:09 +0200

    persist removed

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import styles from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getFilter } from '../../redux/selectors';
+import { deleteContact } from '../../redux/contactsSlice';
+
+function ContactList() {
+  const dispatch = useDispatch();
+  let list = useSelector(getContacts);
+  let filter = useSelector(getFilter);
+
+  function handleClick(evt) {
+    dispatch(deleteContact(evt.target.id));
+  }
+
+  return (
+    <ul>
+      {list
+        .filter(contact => contact.name.toLowerCase().includes(filter))
+        .map(contact => (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button
+              className={styles.delete}
+              onClick={handleClick}
+              id={contact.id}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+export default ContactList;
